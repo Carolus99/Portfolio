93946222c30f6839d85fd036f9d48495f2f86210
Carolus99 <chasecomfort@gmail.com>
2017-01-23 19:19:05 -0800

    init

---
+++ main.js
@@ -0,0 +1,41 @@
+$(function(){
+
+  $.ajax({
+      url: 'https://www.codeschool.com/users/Carolus99.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        console.log('response', response.courses.completed);
+        addCourses(response.courses.completed);
+      }
+    });
+
+function addCourses(courses){
+
+  var $badges = $('#badges');
+
+  courses.forEach(function(course) {
+
+  var $course = $('<div />', {
+    'class':'course'
+  }).appendTo($badges);
+
+  $('<h3 />',{
+    text:course.title
+  }).appendTo($course);
+
+  $('<img />',{
+    src:course.badge
+  }).appendTo($course);
+
+  $('<a />',{
+    'class':'btn btn-primary',
+    target:'_blank',
+    href: course.url,
+    text:'See Course'
+  }).appendTo($course);
+
+
+})
+}  
+
+});
